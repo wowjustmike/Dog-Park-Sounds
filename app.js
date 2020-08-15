8d25396f25fcb3e3f7cee3591f729b627b0c7284
wowjustmike <8887907wowjustmike@users.noreply.github.com>
2020-08-15 10:11:06 -0400

    added fort greene audio file and set to loop

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ new fullpage('#fullpage', {
     onLeave: (origin,destination,direction) => {
         const section = destination.item;
         const title = section.querySelector('h1');
-        const tl = new TimelineMax({delay: 0.4});
+        const tl = new TimelineMax({delay: 0.6});
         tl.fromTo(title,0.5, {y: '50', opacity: 0}, {y: 0, opacity:1})
     }
 })
